57fd6ab657128c7be8979656c58a2de94edd9162
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-01-25 15:48:19 +0530

    Add files via upload

---
+++ ArithmeticOperators.py
@@ -0,0 +1,12 @@
+"""
+Title     : Arithmetic Operators
+Subdomain : Introduction
+Domain    : Python
+Problem   : https://www.hackerrank.com/challenges/python-arithmetic-operators/problem
+"""
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a * b)
